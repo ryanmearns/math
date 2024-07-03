9959500dbd31557ab5a92319fa63340a2e8596c1
Ryan Mearns <ryan.j.mearns@gmail.com>
2024-07-03 04:34:46 +0000

    add multiple

--- index.ts
+++ index.ts
@@ -2,4 +2,7 @@ export class Math {
   public sum(arg1: number, arg2: number): number {
     return arg1 + arg2;
   }
+  public multiple(arg1: number, arg2: number): number {
+    return arg1 * arg2;
+  }
 }
